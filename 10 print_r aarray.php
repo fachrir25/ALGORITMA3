32d3ac607df2a9015e4bd7c658d913caa7865e4d
Fachri Rizki <fachririzki2087@gmail.com>
2024-08-29 11:49:43 +0700

    print_r array

---
+++ 10 print_r aarray.php
@@ -0,0 +1,22 @@
+<?php
+$array = array(
+    'name' => 'John Doe',
+    'age' => 30,
+    'skills' => array('PHP', 'JavaScript', 'HTML')
+);
+
+print_r($array);
+
+print "<p>";
+
+$array = array(
+    'name' => 'Michael jackson',
+    'age' => 28,
+    'skills' => array('PHP', 'JavaScript', 'CSS')
+);
+
+var_dump($array);
+
+print "<p>";
+
+print_r("<i><p><strong>By ; fachri rizki");
